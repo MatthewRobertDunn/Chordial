ffc394e9497c3fe77d7346b0a4fcd3bf1ae61dcd
Dunn, Matthew (DPC) <Matthew.Dunn@sa.gov.au>
2019-07-25 14:17:56 +0930

    Added support for Certificates

---
+++ Hive.Overlay.Peer/Crypto/Keygen.cs
@@ -0,0 +1,92 @@
+﻿using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.Sec;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Operators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
+using System;
+using System.Collections.Generic;
+using MS = System.Security.Cryptography;
+using System.Text;
+using BC = Org.BouncyCastle;
+using Org.BouncyCastle.Asn1.X509;
+using System.IO;
+
+namespace Hive.Overlay.Peer.Crypto
+{
+    public class KeyGen
+    {
+        public MS.X509Certificates.X509Certificate2 Generate()
+        {
+            //create keypair
+            IAsymmetricCipherKeyPairGenerator bcKpGen = GeneratorUtilities.GetKeyPairGenerator("ECDSA");
+            bcKpGen.Init(new ECKeyGenerationParameters(SecObjectIdentifiers.SecP256r1, new SecureRandom()));
+            AsymmetricCipherKeyPair bcSubjKeys = bcKpGen.GenerateKeyPair();
+
+            IAsymmetricCipherKeyPairGenerator issuerGen = GeneratorUtilities.GetKeyPairGenerator("ECDSA");
+            issuerGen.Init(new ECKeyGenerationParameters(SecObjectIdentifiers.SecP256r1, new SecureRandom()));
+            AsymmetricCipherKeyPair issuerKeys = issuerGen.GenerateKeyPair();
+
+            X509Certificate bcCert = CreateCert(bcSubjKeys, issuerKeys, "CN=Hive", "CN=HiveRoot");
+            X509Certificate issuerCert = CreateCert(issuerKeys, issuerKeys, "CN=HiveRoot, O=Bitcoin", "CN=HiveRoot");
+
+
+            //export keypair to a microsoft format in a roundabout way
+            var store = new Pkcs12Store();
+            string friendlyName = bcCert.SubjectDN.ToString();
+
+            var certificateEntry = new X509CertificateEntry(bcCert);
+            var issuerCertEntry = new X509CertificateEntry(issuerCert);
+            store.SetCertificateEntry("issuer", issuerCertEntry);
+            store.SetCertificateEntry(friendlyName, certificateEntry);
+            store.SetKeyEntry(friendlyName, new AsymmetricKeyEntry(bcSubjKeys.Private), new[] { certificateEntry });
+
+            const string password = "password";
+            var stream = new MemoryStream();
+            store.Save(stream, password.ToCharArray(), new SecureRandom());
+
+            File.WriteAllBytes(@"c:\temp\foo.pfx", stream.ToArray());
+
+
+            var bitcoinCert = new MS.X509Certificates.X509Certificate2(issuerCert.GetEncoded(), string.Empty);
+
+            //conver to MS format.
+            var transportCert =
+                new MS.X509Certificates.X509Certificate2(
+                    stream.ToArray(), password,
+                    MS.X509Certificates.X509KeyStorageFlags.PersistKeySet | MS.X509Certificates.X509KeyStorageFlags.Exportable);
+
+
+            var ch = new MS.X509Certificates.X509Chain();
+
+            ch.ChainPolicy.RevocationMode = MS.X509Certificates.X509RevocationMode.NoCheck;
+            ch.ChainPolicy.RevocationFlag = MS.X509Certificates.X509RevocationFlag.ExcludeRoot;
+            ch.ChainPolicy.VerificationFlags = MS.X509Certificates.X509VerificationFlags.AllowUnknownCertificateAuthority;
+
+            ch.ChainPolicy.ExtraStore.Add(bitcoinCert);
+            var isValid = ch.Build(transportCert);
+
+            return transportCert;
+        }
+
+        private static X509Certificate CreateCert(AsymmetricCipherKeyPair bcSubjKeys, AsymmetricCipherKeyPair issuerKeys, string subject, string issuer)
+        {
+            //create cert, sign with private key
+            X509V3CertificateGenerator bcXgen = new X509V3CertificateGenerator();
+            var subjectDN = new X509Name(subject);
+            var issuerDN = new X509Name(issuer);
+            bcXgen.SetIssuerDN(issuerDN);
+            bcXgen.SetSubjectDN(subjectDN);
+            bcXgen.SetNotBefore(DateTime.Now);
+            bcXgen.SetNotAfter(DateTime.Now.AddYears(100));
+            bcXgen.SetPublicKey(bcSubjKeys.Public);
+            bcXgen.SetSerialNumber(new BC.Math.BigInteger(256, new SecureRandom()));
+
+            ISignatureFactory bcSigFac = new Asn1SignatureFactory("SHA256WITHECDSA", issuerKeys.Private);
+            var bcCert = bcXgen.Generate(bcSigFac);
+            return bcCert;
+        }
+    }
+}
--- Hive.Overlay.Peer/Program.cs
+++ Hive.Overlay.Peer/Program.cs
@@ -1,21 +1,42 @@
-﻿using Microsoft.AspNetCore;
+﻿using Hive.Overlay.Peer.Crypto;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Hive.Overlay.Peer
 {
     class Program
     {
+        private static X509Certificate2 Certificate;
         public static void Main(string[] args)
         {
             Console.WriteLine("Hive node starting...");
+
+            Console.WriteLine("Generating certificate");
+            var keygen = new KeyGen();
+            Certificate = keygen.Generate();
+            Console.WriteLine("Starting web server");
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://0.0.0.0:5000")
-                .UseKestrel(options => options.AddServerHeader = false)
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var bitkey = new NBitcoin.Key();
+            var secret = bitkey.GetBitcoinSecret(NBitcoin.Network.TestNet);
+
+
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    options.AddServerHeader = false;
+                    options.Listen(System.Net.IPAddress.Any, 5000, configure =>
+                     {
+                         configure.UseHttps(Certificate);
+
+                     });
+               })
                 .UseStartup<Startup>();
+        }
     }
 }
